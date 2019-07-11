c10a955ef473095dafa6d05f62f8be0ffddcba69
snazarki <unknown>
2019-07-11 17:36:33 +0300

    Jtreg test compiler/uncommontrap/TestDeoptOOM.java causes JVM assert

--- src/cpu/aarch32/vm/templateInterpreter_aarch32.cpp
+++ src/cpu/aarch32/vm/templateInterpreter_aarch32.cpp
@@ -232,18 +232,6 @@ address TemplateInterpreterGenerator::generate_deopt_entry_for(TosState state,
   __ restore_constant_pool_cache();
   __ get_method(rmethod);
 
-  // handle exceptions
-  {
-    Label L;
-    __ ldr(rscratch1, Address(rthread, Thread::pending_exception_offset()));
-    __ cbz(rscratch1, L);
-    __ call_VM(noreg,
-               CAST_FROM_FN_PTR(address,
-                                InterpreterRuntime::throw_pending_exception));
-    __ should_not_reach_here();
-    __ bind(L);
-  }
-
   __ get_dispatch();
 
   // Calculate stack limit
@@ -260,6 +248,18 @@ address TemplateInterpreterGenerator::generate_deopt_entry_for(TosState state,
   // NULL last_sp until next java call
   __ mov(rscratch1, 0);
   __ str(rscratch1, Address(rfp, frame::interpreter_frame_last_sp_offset * wordSize));
+
+  // handle exceptions
+  {
+    Label L;
+    __ ldr(rscratch1, Address(rthread, Thread::pending_exception_offset()));
+    __ cbz(rscratch1, L);
+    __ call_VM(noreg,
+               CAST_FROM_FN_PTR(address,
+                                InterpreterRuntime::throw_pending_exception));
+    __ should_not_reach_here();
+    __ bind(L);
+  }
 
   __ dispatch_next(state, step);
   return entry;
